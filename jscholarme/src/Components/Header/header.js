502389c402e466b6eb19c4a3d115394d588a7628
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-06-25 00:26:28 -0700

    updated footer and header

---
+++ jscholarme/src/Components/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+function header() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+import './header.css'
+
+export default header
